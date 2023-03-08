54e7b45982b4bdd86b309cc1fb6fa9b954c08da4
Jesus <jesusleot10@gmail.com>
2023-03-08 17:05:22 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const hamburgerMenu = document.querySelector('.hamburger');
+const menuMobile = document.querySelector('.header__menu-mobile');
+const background = document.querySelector('.background')
+
+
+hamburgerMenu
+  .addEventListener('click', () => {
+      hamburgerMenu.classList.toggle('active')
+      menuMobile.classList.toggle('open')
+      background.classList.toggle('background__open')
+  });
+
+/* Slideshow functions */
+const showDivs = (n) => {
+  let i;
+  let slides = document.getElementsByClassName("slideshow__content");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length} ;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"
+  }
+  slides[slideIndex-1].style.display = "grid"
+}
+  
+let slideIndex = 1;
+showDivs(slideIndex);
+
+const plusDivs = (n) => {
+  showDivs(slideIndex += n);
+}
